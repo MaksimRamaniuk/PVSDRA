ce45565046c8d39e429675c5e1fa7da86965e9ea
VladislavErmakovich <vladermakovich29@gmail.com>
2025-11-20 17:06:32 +0300

    coef was fixed

--- Lab_3/coef.m
+++ Lab_3/coef.m
@@ -15,8 +15,15 @@ rom2 = zeros(8,1);
 
 for addr = 0:7
     bits = bitget(addr, [1 2 3]);
-    rom1(addr+1) = bits(1)*h1(1) + bits(2)*h1(2) + bits(3)*h1(3);
-    rom2(addr+1) = bits(1)*h2(1) + bits(2)*h2(2) + bits(3)*h2(3);
+    for i = 1:3
+        if bits(i) == 1 
+            bits(i) = -1;
+        else
+            bits(i) = 1;
+        end
+    end
+    rom1(addr+1) = (-1/2)*(bits(1)*h1(1) + bits(2)*h1(2) + bits(3)*h1(3));
+    rom2(addr+1) = (-1/2)*(bits(1)*h2(1) + bits(2)*h2(2) + bits(3)*h2(3));
 end
 
 Q = 2^(Order-1);
@@ -24,10 +31,10 @@ rom1_q = round(rom1 * Q);
 rom2_q = round(rom2 * Q);
 
 fprintf('ROM1 (Q1.11):\n');
-disp(rom1_q.');
+disp(rom1.');
 
 fprintf('ROM2 (Q1.11):\n');
-disp(rom2_q.');
+disp(rom2.');
 
 x = zeros(1, 8);
 x(1) = 1;
